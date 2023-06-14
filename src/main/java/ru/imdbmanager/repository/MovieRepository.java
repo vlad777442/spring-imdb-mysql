8e0e857279c1fad63073d3881b0df8ec07761725
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-06-14 07:09:13 +0300

    1.2 deleted unused imports

--- src/main/java/ru/imdbmanager/repository/MovieRepository.java
+++ src/main/java/ru/imdbmanager/repository/MovieRepository.java
@@ -3,13 +3,9 @@ package ru.imdbmanager.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import ru.imdbmanager.model.Genre;
 import ru.imdbmanager.model.Movie;
 
-import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
